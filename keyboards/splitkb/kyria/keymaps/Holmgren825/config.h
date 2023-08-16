e55b6acb503384a7779960e6bb2fa982cdf004bd
Erik Holmgren <erikholmgren24@gmail.com>
2023-08-16 19:20:54 +0200

    New keymap. Includes homerow mods.

--- keyboards/splitkb/kyria/keymaps/Holmgren825/config.h
+++ keyboards/splitkb/kyria/keymaps/Holmgren825/config.h
@@ -31,5 +31,5 @@
 #define EE_HANDS
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 #define SPLIT_USB_DETECT
-#define NO_USB_STARTUP_CHECK
+// #define NO_USB_STARTUP_CHECK
 #define TAPPING_TOGGLE 2
--- keyboards/splitkb/kyria/keymaps/Holmgren825/rules.mk
+++ keyboards/splitkb/kyria/keymaps/Holmgren825/rules.mk
@@ -1,4 +1,3 @@
 OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
-TAP_DANCE_ENALBE = yes    # Tap dance
